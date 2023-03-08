01c02b68aa11011954a62de66e765929f02ae921
Frederik Bode <frederik@paperbox.ai>
2023-03-08 16:15:42 +0100

    add exponential backoff

--- src/sparp/sparp.py
+++ src/sparp/sparp.py
@@ -1,21 +1,33 @@
 import asyncio
 import aiohttp
 import aiohttp.client_exceptions
+import aiohttp_retry
 import time
 from typing import Dict, List
+from aiohttp_retry import RetryClient, ExponentialRetry
+import logging
+from aiohttp import TraceConfig
+
+
+async def on_request_start(session, trace_config_ctx, params) -> None:
+    current_attempt = trace_config_ctx.trace_request_ctx['current_attempt']
+    if current_attempt > 1:
+        print(f"Retrying request, attempt number {current_attempt}")
 
 
 class SharedMemory:
-    def __init__(self, total, cols=40):
+    def __init__(self, total, cols=40, disable_bar=False):
         self.lock = asyncio.Lock()
         self.done = 0
         self.success = 0
         self.fail = 0
         self.total = total
-        self.cols = 40
+        self.cols = cols
         self.start_time = time.time()
         self.should_stop = False
-        self.print_counter()
+        self.disable_bar = disable_bar
+        if not self.disable_bar:
+            self.print_counter()
 
     async def set_should_stop(self):
         async with self.lock:
@@ -36,11 +48,13 @@ class SharedMemory:
         async with self.lock:
             self.done += 1
             self.fail += 1
-            self.print_counter()
+            if not self.disable_bar:
+                self.print_counter()
 
     async def update(self):
         async with self.lock:
-            self.print_counter()
+            if not self.disable_bar:
+                self.print_counter()
 
     async def check_done(self):
         async with self.lock:
@@ -49,7 +63,6 @@ class SharedMemory:
 
     def print_counter(self, done=False):
         elapsed = time.time() - self.start_time
-        # print(f"total = {self.value} / {self.total}, took {elapsed}", end="\r", flush=True)
         percent = int(self.done / self.total * self.cols)
         remainder = self.cols - percent
         full = ''.join(['=' for _ in range(percent)])
@@ -99,12 +112,23 @@ async def updater(shared):
         done = await shared.check_done()
         should_stop = await shared.get_should_stop()
         if done or should_stop:
-            shared.print_counter(done=True)
+            if not shared.disable_bar:
+                shared.print_counter(done=True)
             break
 
 
-async def async_main(source_queue, shared, max_outstanding_requests, ok_status_codes, stop_on_first_fail):
-    async with aiohttp.ClientSession(skip_auto_headers=["Content-Type"]) as session:
+async def async_main(source_queue, shared, max_outstanding_requests, ok_status_codes, stop_on_first_fail, retry_attempts, retry_status_codes):
+    trace_config = TraceConfig()
+    trace_config.on_request_start.append(on_request_start)
+    async with RetryClient(
+            client_session=aiohttp.ClientSession(
+                skip_auto_headers=["Content-Type"], trace_configs=[trace_config]),
+            retry_options=ExponentialRetry(
+                attempts=retry_attempts,
+                statuses=set(retry_status_codes),
+                retry_all_server_errors=False
+            ),
+            raise_for_status=False) as session:
         coros = [updater(shared)] + [consumer(source_queue, session, shared, ok_status_codes, stop_on_first_fail)
                                      for _ in range(max_outstanding_requests)]
         results = await asyncio.gather(*coros)
@@ -119,7 +143,7 @@ async def fill_queue(queue, items):
     return queue
 
 
-def sparp(configs: List[Dict], max_outstanding_requests: int, ok_status_codes=[200], stop_on_first_fail=False) -> List:
+def sparp(configs: List[Dict], max_outstanding_requests: int, ok_status_codes=[200], stop_on_first_fail=False, disable_bar: bool = False, retry_attempts: int = 1, retry_status_codes=[]) -> List:
     """Simple Parallel Asynchronous Requests in Python
 
     Arguments:
@@ -127,13 +151,15 @@ def sparp(configs: List[Dict], max_outstanding_requests: int, ok_status_codes=[2
       max_outstanding_requests (int): max number of parallel requests alive at the same time
       ok_status_codes (List[int]): list of status codes deemed "success"
       stop_on_first_fail (bool): whether or not to stop sending requests if we get a status not in stop_on_first_fail
+      disable_bar (bool): do not print anything
+      retry_attempts (int): number of times to retry with exponential backoff
 
     Returns:
       List: list of Responses
     """
     source_queue = asyncio.Queue()
     source_queue = asyncio.run(fill_queue(source_queue, configs))
-    shared = SharedMemory(total=len(configs))
+    shared = SharedMemory(total=len(configs), disable_bar=disable_bar)
     result = asyncio.run(async_main(source_queue, shared,
-                         max_outstanding_requests, ok_status_codes, stop_on_first_fail))
+                         max_outstanding_requests, ok_status_codes, stop_on_first_fail, retry_attempts, retry_status_codes))
     return result
